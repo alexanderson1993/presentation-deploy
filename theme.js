e41a98ce44602ca0e77a8907e228a404175f6920
Alex Anderson <alexanderson1993@gmail.com>
2019-03-07 12:50:01 -0700

    Initial Commit

---
+++ theme.js
@@ -0,0 +1,16 @@
+import { dark } from "mdx-deck/themes";
+
+export default {
+  ...dark,
+  p: {
+    lineHeight: 2
+  },
+  pre: {
+    lineHeight: 2
+  }
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+};
